21f10b00ac4a0907a4d6e618233c19ab27f17ceb
Robert Baumgarten <112660207+rbaumgarten1119@users.noreply.github.com>
2025-10-30 13:25:54 -0400

    Delete sam-cpu-lidar-branch/roboboat-lidar/build/build directory

--- sam-cpu-lidar-branch/roboboat-lidar/build/build/my_lidar_processor/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/phoenixautonomy/ros2_ws/sam-cpu-lidar-branch/roboboat-lidar/src/my_lidar_processor
-# Build directory: /home/phoenixautonomy/ros2_ws/sam-cpu-lidar-branch/roboboat-lidar/build/my_lidar_processor
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
