a9f825a78d915919d7db6facfc6833bc399e0732
Robert Baumgarten <112660207+rbaumgarten1119@users.noreply.github.com>
2025-10-30 13:00:19 -0400

    Added first components of build folder

---
+++ build/lidar_csv_logger/share/lidar_csv_logger/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/home/phoenixautonomy/ros2_ws/build/lidar_csv_logger"
---
+++ build/my_lidar_processor/CMakeFiles/lidar_processor_3d_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lidar_processor_3d_node.dir/src/lidar_processor_3d_node.cpp.o"
+  "CMakeFiles/lidar_processor_3d_node.dir/src/lidar_processor_3d_node.cpp.o.d"
+  "lidar_processor_3d_node"
+  "lidar_processor_3d_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lidar_processor_3d_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
